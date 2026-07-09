df718f5f9b18ee7d14504f1613d9fe0d6df3abf8
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-09 15:52:00 +0300

    add load customer middleware

--- middlewares/loadCustomer.js
+++ middlewares/loadCustomer.js
@@ -1,5 +1,28 @@
+import { loadJson } from "./jsonRepo.js";
 
+export default async (req, res, next) => {
+    try {
+        const allCustomers = await loadJson("/customers.json")
+        const customerId = req.query.customerId;
 
-export default (req, res, next) => {
-    
+        if (!customerId) {
+        return res
+            .status(400)
+            .send({ success: false, message: `customerId not found`});
+        }
+
+        const customer = allCustomers.find(c => c.customerId === +customerId)
+
+        if (!customer) {
+        return res
+            .status(400)
+            .send({ success: false, message: `Custom ${customerId} not exists`});
+        }
+
+        req.customer = customer
+
+    } catch (error) {
+        res.status(500).send({"success": false, "message": "Server internal error"})
+        console.log("unable find user", error);
+    }
 }
